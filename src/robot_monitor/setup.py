5dc2df51e41c270b63d6c3a4fdb9eae22c1c140e
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-16 17:49:22 +0800

    updated setup for robot monitor

--- src/robot_monitor/setup.py
+++ src/robot_monitor/setup.py
@@ -1,15 +1,18 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'robot_monitor'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'action'), glob('action/*.action'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'battery_server = robot_monitor.battery_action_server:main',
+            'battery_client = robot_monitor.battery_action_client:main'
         ],
     },
 )
