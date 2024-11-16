62d9c8ac9d55dae1f37bce35538c626b79354334
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-16 15:25:44 +0800

    added action server implementation

---
+++ src/robot_monitor/robot_monitor/battery_action_client.py
@@ -0,0 +1,54 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+from robot_monitor.action import Charge
+import sys
+
+
+class BatteryActionClient(Node):
+    def __init__(self):
+        super().__init__('battery_action_client')
+        self._action_client = ActionClient(self, Charge, 'charge_battery')
+
+    def send_goal(self, target_level):
+        goal_msg = Charge.Goal()
+        goal_msg.target_battery_level = target_level
+        self._action_client.wait_for_server()
+        self.get_logger().info('Sending charging goal')
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg,
+            feedback_callback=self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected')
+            return
+        self.get_logger().info('Goal accepted')
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(
+            f'Final battery level: {result.final_battery_level}%,'
+            f'Charging time: {result.total_charging_time} seconds')
+        rclpy.shutdown()
+
+    def feedback_callback(self,feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info(
+            f'Current level: {feedback.current_battery_level}%, '
+            f'Charging rate: {feedback.charging_rate}%/min')
+
+
+def main(args=None):
+    rclcp.init(args=args)
+    action_client = BatteryActionClient()
+    action_client.send_goal(90.0)
+    rclpy.spin(action_client)
+    action_server.destroy_node()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/robot_monitor/robot_monitor/battery_action_server.py
@@ -0,0 +1,64 @@
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+from robot_monitor.action import Charge
+import time
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+
+
+class BatteryActionServer(Node):
+    def __init__(self):
+        super().__init__('battery_action_server')
+        self._action_server = ActionServer(self, Charge, 'charge_battery',
+            self.execute_callback, callback_group=ReentrantCallbackGroup())
+        self.current_battery = 20.0
+        self.charging_rate = 10.0
+        self.get_logger().info('Battery action server initialized')
+
+    async def execute_callback(self):
+        self.get_logger().info('Received charging goal')
+        feedback_msg = Charge.Feedback()
+        result = Charge.Result()
+        start_time = time.time()
+        if goal_handle.request.target_battery_level > 100.0:
+            goal_handle.abort()
+            return result
+        while self.current_battery < goal_handle.request.target_battery_level:
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                result.final_battery_level = self.current_battery
+                result.total_charging_time = time.time() - start_time
+                return result
+            self.current_battery += (self.charging_rate / 60.0)
+            if self.current_battery > 100.0:
+                self.current_battery = 100.0
+            feedback_msg.current_battery_level = self.current_battery
+            feedback_msg.charging_rate = self.charging_rate
+            goal_handle.publish_feedback(feedback_msg)
+            await self.sleep(1)
+        goal_handle.succeed()
+        result.final_battery_level = self.current_battery
+        result.total_charging_time = time.time() - start_time
+        return result
+
+    async def sleep(self, seconds):
+        await self.create_rate(1 / seconds).timer.wait()
+
+
+def main(args=None):
+    rclcp.init(args=args)
+    action_server = BatteryActionServer()
+    executor = MultiThreadedExecutor()
+    executor.add_node(action_server)
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        action_server.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
